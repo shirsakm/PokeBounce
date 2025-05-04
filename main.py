67fb68ed3bd6ac8eb4e61e86f3361105b0078224
LeBoufty <boufto@gmail.com>
2025-05-04 12:57:43 +0200

    Put import back in its place

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 from src.constants import WINDOW_HEIGHT, WINDOW_WIDTH
 from src.globals import g
-from src.game import Game
 
 pygame.init()
 FPS = 60
@@ -9,6 +8,8 @@ fpsClock = pygame.time.Clock()
 g.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.Surface.convert_alpha(g.window)
 pygame.display.set_caption('Auto Arena')
+
+from src.game import Game
 
 game = Game()
 while True:
